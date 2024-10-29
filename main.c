81cbc8b4584045db0965f21ce133392f077d4d70
Zimgo012 <rjccabelcina@gmail.com>
2024-10-29 14:50:57 -0400

    initial functionality added

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+
+void printMap();
 /*Map*/
 int mapWidth;
 int mapHeight;
@@ -19,10 +21,11 @@ int digCoordX;
 int digCoordY;
 
 /*Situation*/
-bool isTreasureFound;
+bool isAllTreasureFound;
+int numberOfTreasuresFound;
 
 /*Setting*/
-int cheatModeToggle;
+bool isCheatModeOn;
 
 
 /*Loop Variables for looping*/
@@ -34,14 +37,11 @@ int y;
 #define MAX_CODE_NUMBER 4
 int main(void) {
 
-
-
     /*Intro*/
     printf("Enter Map Width\n");
     scanf("%d", &mapWidth);
     printf("Enter Map Height\n");
     scanf("%d", &mapHeight);
-    /**/
     printf("Enter Number of Treasures\n");
     scanf("%d", &numTreasures);
 
@@ -71,14 +71,78 @@ int main(void) {
         printf("Enter treasure #%d code: ", i+1);
         scanf("%s", treasures[i]);
 
-        map[randomXAxis][randomYAxis] = treasures[i];
+        map[randomYAxis][randomXAxis] = treasures[i];
 
     }
 
+    printMap();
+
+    while(!isAllTreasureFound) {
+        /*Menu*/
+        printf("Enter Choice: \n");
+        printf("1 - Dig at spot\t| 2 - Exit Game\t| 3 - Cheat!\n");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                /*Sub Menu*/
+                 printf("Where would you like to dig?\n");
+                 printf("X = ");
+                 scanf("%d",&digCoordX);
+                 printf("Y = ");
+                 scanf("%d",&digCoordY);
+
+            /*Perform checking for each treasure if its equal to the digging coordinates*/
+                for(i = 0; i < numTreasures; i++) {
+                if(map[digCoordY][digCoordX] == treasures[i]) {
+                    printf("Treasure #%d found!\n", i+1);
+                    numberOfTreasuresFound++;
+                    map[digCoordY][digCoordX] = "$";
+                }else if(map[digCoordY][digCoordX] == NULL){
+                    printf("You found nothing!\n");
+                    map[digCoordY][digCoordX] = "#";
+                }
+            }
+                break;
+            case 2:
+                printf("Thank you for playing");
+                return EXIT_SUCCESS;
+            case 3:
+                isCheatModeOn = true;
+                printf("Cheat Mode Enabled\n");
+                break;
+            default:
+                printf("Wrong choice\n");
+        }
+        /*All treasures found!*/
+        if(numberOfTreasuresFound == numTreasures) {
+            isAllTreasureFound = true;
+            printf("Congratulations, you found all the treasure!\n");
+        }
+
+        /*Show map here*/
+        printMap();
+    }
+
+
+    /*Free*/
+    /*Freeing Map*/
+    for(i = 0; i < mapHeight; i++) {
+        free(map[i]);
+    }
+    for(i = 0; i < numTreasures; i++) {
+        free(treasures[i]);
+    }
+    free(map);
+    /*Freeing Treasure*/
+    free(treasures);
 
 
 
+return EXIT_SUCCESS;
+}
 
+void printMap() {
     /*Printing Map*/
     printf("Map: \n");
 
@@ -106,33 +170,21 @@ int main(void) {
         printf("%d|",y%10);
         /*Printing map*/
         for (x=0; x < mapWidth; x++) {
-            if(map[y][x] != NULL) {
-                printf("x");
+            if(map[y][x] == NULL) {
+                printf(" ");
+            }else if(map[y][x] == "#") {
+                printf("#");
+            }else if(map[y][x] == "$") {
+                printf("$");
+            }else if(isCheatModeOn && map[y][x] != NULL) {
+                printf("!");
             }else {
-                printf(".");
+                printf(" ");
             }
-
         }
         printf("\n");
 
     }
 
-
-
-    /*Free*/
-    /*Freeing Map*/
-    for(i = 0; i < mapWidth; i++) {
-        free(map[i]);
-    }
-    for(i = 0; i<numTreasures; i++) {
-        free(treasures[i]);
-    }
-    free(map);
-    /*Freeing Treasure*/
-    free(treasures);
-
-
-
-return EXIT_SUCCESS;
 }
 
